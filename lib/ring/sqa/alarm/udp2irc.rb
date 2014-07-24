a94934fe2ff2cbfdb79f2249b56498433ea22aa6
Saku Ytti <saku@ytti.fi>
2014-07-24 12:10:10 +0300

    fix UDP2IRC config variables

--- lib/ring/sqa/alarm/udp2irc.rb
+++ lib/ring/sqa/alarm/udp2irc.rb
@@ -3,7 +3,7 @@ class SQA
 class Alarm
 
   class UDP2IRC
-    def send message, channel=CFG.alarm.irc.channel
+    def send message, channel=CFG.irc.channel
       msg = [@password, channel, message[:short]].join ' '
       msg += "\0" while msg.size % 16 > 0
       UDPSocket.new.send msg, 0, HOST, PORT
@@ -11,7 +11,7 @@ class Alarm
 
     private
 
-    def initialize host=CFG.alarm.irc.host, port=CFG.alarm.irc.port, password=CFG.alarm.irc.password
+    def initialize host=CFG.irc.host, port=CFG.irc.port, password=CFG.irc.password
       @host     = host
       @port     = port
       @password = password
