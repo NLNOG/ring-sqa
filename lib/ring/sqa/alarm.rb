18f0141716a897cf77e9eca23835826b9ec13b5a
Saku Ytti <saku@ytti.fi>
2014-07-25 13:19:30 +0300

    move alarm message to own file
    + fix CFG:method to CFG.method in poller.rb

--- lib/ring/sqa/alarm.rb
+++ lib/ring/sqa/alarm.rb
@@ -1,6 +1,7 @@
 require_relative 'alarm/email'
 require_relative 'alarm/udp2irc'
 require_relative 'alarm/cfg'
+require_relative 'alarm/message'
 require_relative 'mtr'
 require_relative 'paste'
 
@@ -34,8 +35,8 @@ class SQA
       @methods  = []
       @methods  << Email.new   if CFG.email.to?
       @methods  << UDP2IRC.new if CFG.irc.password?
+      @hostname = Ring::SQA::CFG.host.name
       @alarm    = false
-      @hostname = (Socket.gethostname rescue 'anonymous')
     end
 
     def compose_message alarm_buffer
@@ -63,40 +64,7 @@ class SQA
         time -= 1
       end
 
-      msg[:long] = <<EOF
-Regarding: #{@hostname}
-
-This is an automated alert from the distributed partial outage
-monitoring system "RING SQA".
-
-At #{Time.now.utc} the following measurements were analysed
-as indicating that there is a high probability your NLNOG RING node
-cannot reach the entire internet. Possible causes could be an outage
-in your upstream's or peer's network.
-
-The following #{exceeding_nodes.size} nodes previously were reachable, but became unreachable
-over the course of the last 3 minutes:
-
-#{nodes_list}
-
-As a debug starting point 3 traceroutes were launched right after
-detecting the event, they might assist in pinpointing what broke:
-
-#{mtr_list}
-
-An alarm is raised under the following conditions: every 30 seconds
-your node pings all other nodes. The amount of nodes that cannot be
-reached is stored in a circular buffer, with each element representing
-a minute of measurements. In the event that the last three minutes are
-#{Ring::SQA::CFG.analyzer.tolerance} above the median of the previous 27 measurement slots, a partial
-outage is assumed. The ring buffer's output is as following:
-
-#{buffer_list}
-
-Kind regards,
-
-NLNOG RING
-EOF
+      msg[:long] = message nodes_list, mtr_list, buffer_list
       msg
     end
 
