aea7ae262cc90acf1aa01bfdb25dab47f0b17c6a
Saku Ytti <saku@ytti.fi>
2014-07-29 15:01:27 +0300

    irc init no longer needed, nor wanted

--- lib/ring/sqa/alarm/udp2irc.rb
+++ lib/ring/sqa/alarm/udp2irc.rb
@@ -18,12 +18,6 @@ class Alarm
 
     private
 
-    def initialize host=CFG.irc.host, port=CFG.irc.port, password=CFG.irc.password
-      @host     = host
-      @port     = port
-      @password = password
-    end
-
     def udp2irc password, target, message, url, host, port
       msg = [password, target, message, url].join ' '
       msg += "\0" while msg.size % 16 > 0
