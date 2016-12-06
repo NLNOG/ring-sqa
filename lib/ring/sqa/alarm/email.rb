23f207dc4b8d9f6c0e217690f89c7bf6a170f388
Saku Ytti <saku@ytti.fi>
2016-12-06 10:53:47 +0200

    allow configurable list_id
    closes #16

--- lib/ring/sqa/alarm/email.rb
+++ lib/ring/sqa/alarm/email.rb
@@ -5,13 +5,15 @@ class SQA
 class Alarm
 
   class Email
-    SERVER = 'localhost'
+    SERVER  = 'localhost'
+    LIST_ID = 'ring-sqa <sqa.ring.nlnog.net'
 
     def send opts
       short, long = opts[:short], opts[:long]
       @from     = CFG.email.from
       @to       = [CFG.email.to].flatten
       prefix    = CFG.email.prefix? ? CFG.email.prefix : ''
+      @list_id  = CFG.email.list_id? ? CFG.email.list_id : LIST_ID
       @subject  = prefix + short
       @reply_to = CFG.email.reply_to? ? CFG.email.reply_to : @from
       @body     = long
@@ -28,7 +30,7 @@ class Alarm
       mail << 'To: '       + @to.join(', ')
       mail << 'Reply-To: ' + @reply_to
       mail << 'Subject: '  + @subject
-      mail << 'List-Id: '  + 'ring-sqa <sqa.ring.nlnog.net>'
+      mail << 'List-Id: '  + @list_id
       mail << 'X-Mailer: ' + 'ring-sqa'
       mail << ''
       mail = mail.join("\n")
