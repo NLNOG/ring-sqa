aabd63e514c3cae7da14ac644f8a59b7048a7d8d
Nick Douma <nick.douma@cyso.com>
2025-06-03 12:11:50 +0200

    Add facility for separate web server access logs

--- lib/ring/sqa/log.rb
+++ lib/ring/sqa/log.rb
@@ -4,13 +4,19 @@ class SQA
   if CFG.debug?
     require 'logger'
     Log = Logger.new STDERR
+    Log.level = Logger::DEBUG
+    AccessLog = Log
   else
     begin
       require 'syslog/logger'
       Log = Syslog::Logger.new 'ring-sqad%i' % ( CFG.afi == "ipv6" ? 6 : 4 )
+      Log.level = Logger::INFO
+      AccessLog = Logger.new STDERR
     rescue LoadError
       require 'logger'
       Log = Logger.new STDERR
+      Log.level = Logger::INFO
+      AccessLog = Log
     end
   end
 
